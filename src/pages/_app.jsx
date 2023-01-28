3afb08e92e4f81c4a1b1cafc15121d5e64484cdd
carlosreyesweb <cgrt17062011@gmail.com>
2023-01-28 18:15:56 -0400

    old font loading implementation

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,16 +1,5 @@
 import '@/styles/globals.css'
-import { Jua, Nunito, Open_Sans } from '@next/font/google'
-
-const jua = Jua({ weight: '400', subsets: ['latin'] })
-const nunito = Nunito({ weight: ['600', '700'], subsets: ['latin'] })
-const openSans = Open_Sans({ weight: ['400', '500'], subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return (
-    <main
-      className={`${jua.className} ${nunito.className} ${openSans.className}`}
-    >
-      <Component {...pageProps} />
-    </main>
-  )
+  return <Component {...pageProps} />
 }
