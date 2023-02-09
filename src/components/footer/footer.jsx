9dc444f7d7eeb48dd7fcfc20f79b4101eeadac17
carlosreyesweb <cgrt17062011@gmail.com>
2023-02-08 21:11:59 -0400

    Add footer content
    Re-add layout left and right padding

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,21 @@
+import SocialMedia from '../social-media/social-media'
 import styles from './footer.module.css'
 
 export default function Footer(props) {
-  return <footer className={styles.container}></footer>
+  return (
+    <footer className={styles.container}>
+      <p className={styles.copyright}>
+        &copy; 2023 | ModalMilk | hecho con &hearts; por{' '}
+        <a
+          href="https://carlosreyesweb.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          carlosreyesweb
+        </a>
+        .
+      </p>
+      <SocialMedia />
+    </footer>
+  )
 }
