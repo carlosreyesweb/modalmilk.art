0f71e5e9a3a8a1d452885f06fff07bd91020574a
carlosreyesweb <cgrt17062011@gmail.com>
2023-02-08 23:55:20 -0400

    Add lightbox
    Remove carets from dependencies

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
-import { useEffect } from 'react'
+import Lightbox from 'yet-another-react-lightbox'
+import 'yet-another-react-lightbox/styles.css'
 import styles from './portfolio.module.css'
+import { useLightbox } from './use-lightbox'
 
 export default function Portfolio({ illustrations }) {
-  useEffect(() => {
-    console.log(illustrations)
-  }, [illustrations])
+  const lb = useLightbox(illustrations)
 
   return (
     <section id="#portafolio" className={styles.container}>
       <h2 className={styles.title}>Portafolio</h2>
       <div className={styles.grid}>
-        {illustrations.map((illustration) => (
-          <article className={styles.gridItem} key={illustration.uuid}>
+        {illustrations.map((illustration, index) => (
+          <article
+            className={styles.gridItem}
+            key={illustration.uuid}
+            onClick={() => lb.openLightbox(index)}
+          >
             <Image
               src={illustration.image}
               alt={illustration.name}
@@ -20,10 +24,17 @@ export default function Portfolio({ illustrations }) {
               className={styles.image}
               width={illustration.width}
               height={illustration.height}
+              priority
             />
           </article>
         ))}
       </div>
+      <Lightbox
+        index={lb.index}
+        open={lb.open}
+        close={lb.closeLightbox}
+        slides={lb.slides}
+      />
     </section>
   )
 }
