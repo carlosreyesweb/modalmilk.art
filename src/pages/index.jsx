13ded9fbaed79bab0955665dcbcbce5f64e39833
carlosreyesweb <cgrt17062011@gmail.com>
2023-02-08 21:02:18 -0400

    Enhance page layout
    Add space between page sections using Grids instead of margins

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Welcome from '@/components/welcome/welcome'
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
           content="¡Hola! Bienvenido a mi sitio web. Soy ModalMilk, una artista especializada en dibujos a comisión. Echa un vistazo a mi galería en línea para ver mis creaciones. Si quieres algo especial, ¡no dudes en encargar una obra de arte hecha a medida solo para ti!"
         />
       </Head>
-      <main>
+      <main className={styles.main}>
         <Welcome />
       </main>
     </>
