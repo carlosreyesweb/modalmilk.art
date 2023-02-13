7e9cfcc0a046df5ac505c472d9df0074d1b05cbe
carlosreyesweb <cgrt17062011@gmail.com>
2023-02-13 19:11:24 -0400

    Create Main component from index layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
+import Main from '@/components/main/main'
 import Portfolio from '@/components/portfolio/portfolio'
 import Welcome from '@/components/welcome/welcome'
 import { getProjects } from '@/services/storyblok.service'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home({ projects }) {
   return (
@@ -14,10 +14,10 @@ export default function Home({ projects }) {
           content="¡Hola! Bienvenido a mi sitio web. Soy ModalMilk, una artista especializada en dibujos a comisión. Echa un vistazo a mi galería en línea para ver mis creaciones. Si quieres algo especial, ¡no dudes en encargar una obra de arte hecha a medida solo para ti!"
         />
       </Head>
-      <main className={styles.main}>
+      <Main>
         <Welcome />
         <Portfolio projects={projects} />
-      </main>
+      </Main>
     </>
   )
 }
---
+++ src/pages/precios.jsx
@@ -0,0 +1,14 @@
+import Main from '@/components/main/main'
+import Head from 'next/head'
+
+export default function Pricing(props) {
+  return (
+    <>
+      <Head>
+        <title>Precios - ModalMilk</title>
+        <meta name="description" content="" />
+      </Head>
+      <Main></Main>
+    </>
+  )
+}
